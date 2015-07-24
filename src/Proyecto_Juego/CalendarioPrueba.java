8d3514a2980519c7698c8a1433d6acc26b69dc19
Luis96791 <luis.fajardozap@hotmail.com>
2015-07-24 10:19:40 -0600

    Fecha Actual agregado

---
+++ src/Proyecto_Juego/CalendarioPrueba.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto_Juego;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author LEFF
+ */
+public class CalendarioPrueba {
+    public static void main(String[] args) {
+        Calendar nuevo = Calendar.getInstance();
+        
+        System.out.println(nuevo.getTime());
+    }
+}
